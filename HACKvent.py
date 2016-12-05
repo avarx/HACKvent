f850850cc4d6158110d7b93a8ffd5ebccdb6846a
Adrian von Arx <a.vonarx@frappant.ch>
2016-12-05 14:15:14 +0100

    get_flag & scan_qr

--- HACKvent.py
+++ HACKvent.py
@@ -2,13 +2,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 from bs4 import BeautifulSoup
+from PIL import Image
+import datetime
+import io
 import json
 import requests
 import sys
+import urllib2 as urllib
+import zbarlight
 
 HV_URL = 'http://hackvent.hacking-lab.com'
-HV_TICKET = 'YOURTICKET'
-HV_USER = 'YOURNICK'
+HV_TICKET = 'YOUR_TICKET'
+HV_USER = 'YOUR_USERNAME'
 HV_COOKIE = {'HACKvent_Ticket': HV_TICKET,'HACKvent_User':HV_USER}
 HV_SERIVCEURL = 'http://hackvent.hacking-lab.com/load.php?service=';
 
@@ -33,3 +38,17 @@ def submit_flag(flag):
     r = requests.post(HV_SERIVCEURL+'solution', data=data,cookies=HV_COOKIE)
     answer = json.loads(r.text)
     return(answer['txt'])
+
+def get_flag(solution):
+    data = {'code':solution, 'day':datetime.datetime.now().day, 'ticket':HV_TICKET}
+    r = requests.post(HV_SERIVCEURL+'getBall', data=data)
+    answer = json.loads(r.text)
+    qr = answer['img']
+    return scan_qr(qr)
+
+def scan_qr(qr):
+    fd = urllib.urlopen(HV_URL+'/img/'+qr)
+    image_file = io.BytesIO(fd.read())
+    im = Image.open(image_file)
+    code = zbarlight.scan_codes('qrcode', im)
+    return code[0]
