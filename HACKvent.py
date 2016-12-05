9da515b07d1c1b0e0441936e3d15731be3fc41da
Adrian von Arx <a.vonarx@frappant.ch>
2016-12-05 13:33:43 +0100

    submit_flag

--- HACKvent.py
+++ HACKvent.py
@@ -2,12 +2,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 from bs4 import BeautifulSoup
+import json
 import requests
 import sys
 
 HV_URL = 'http://hackvent.hacking-lab.com'
 HV_TICKET = 'YOURTICKET'
-HV_COOKIE = {'HACKvent_Ticket': HV_TICKET}
+HV_USER = 'YOURNICK'
+HV_COOKIE = {'HACKvent_Ticket': HV_TICKET,'HACKvent_User':HV_USER}
+HV_SERIVCEURL = 'http://hackvent.hacking-lab.com/load.php?service=';
 
 def get_url(url):
     if HV_TICKET!='YOURTICKET':
@@ -24,3 +27,9 @@ def get_challenge_content(day):
     desc = "Challenge Description: Day "+str(day)
     content = soup.find(text=desc).find_next("div")
     return content
+
+def submit_flag(flag):
+    data = {'code':flag}
+    r = requests.post(HV_SERIVCEURL+'solution', data=data,cookies=HV_COOKIE)
+    answer = json.loads(r.text)
+    return(answer['txt'])
